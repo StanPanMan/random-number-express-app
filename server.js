1f8b92ded0468086c046650fad84f0b1510fe5d8
Staley Reed <reedstaley@gmail.com>
2025-03-17 16:20:39 -0400

    basic express api w/ random numbers

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 3003;
+const N_NUMBERS = 17;
+
+const origins = [
+    /https?:\/\/localhost(:\d+)?/,
+    "https://www.google.com",
+    /^https:\/\/(.*\.)*vccs\.edu$/
+];
+const corsOpts = {
+    origin: origins,
+};
+
+app.use(cors(corsOpts));
+
+function getRandom() {
+    // rand from 0 to 100
+    return Math.floor(Math.random() * 101);
+}
+
+app.get("/", (req, res) => {
+    res.json(Array.from({ length: N_NUMBERS }, getRandom));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+});
